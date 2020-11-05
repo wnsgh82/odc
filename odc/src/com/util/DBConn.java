78758c7505f989754f7237d912ddb7298ebcdf72
sys <sys@LAPTOP-EHKFTRC6>
2020-11-05 15:16:42 +0900

    asdf

--- odc/src/com/util/DBConn.java
+++ odc/src/com/util/DBConn.java
@@ -13,7 +13,7 @@ public class DBConn {
 	public static Connection getConnection() {
 		// String url="jdbc:oracle:thin:@127.0.0.1:1521:ORCL";
 		//String url="jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String url="jdbc:oracle:thin:@211.238.142.107/xe"; //포트번호, 포트아이디
+		String url="jdbc:oracle:thin:@localhost:1521/XE"; //포트번호, 포트아이디
 		String user="ejo";
 		String pwd="java$!";
 		
